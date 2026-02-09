f6c3cf8a802c73c37072cbd8246862a8ff5b04b9
cfinlayson@paxata.com <cfinlayson@paxata.com>
2026-02-09 03:32:58 +0000

    Refactor source and schema YAML for staging models and update `manual_test` pipeline structure

--- pipelines/manual_test.py
+++ pipelines/manual_test.py
@@ -1,17 +1,13 @@
 with DAG():
-    OrchestrationSource_0 = SourceTask(
-        task_id = "OrchestrationSource_0", 
-        component = "OrchestrationSource", 
-        kind = "OnedriveSource", 
-        connector = Connection(kind = "onedrive"), 
-        isNew = True, 
-        format = CSVFormat(
-          allowLazyQuotes = False, 
-          allowEmptyColumnNames = True, 
-          separator = ",", 
-          nullValue = "", 
-          encoding = "UTF-8", 
-          header = True
-        ), 
-        fileOperationProperties = {}
+    zsgd_acct = Task(
+        task_id = "zsgd_acct", 
+        component = "Dataset", 
+        writeOptions = {"writeMode" : "overwrite"}, 
+        table = {"name" : "zsgd_acct", "sourceName" : "westpac_edw_views", "sourceType" : "Table"}
+    )
+    zsgd_acct_loan = Task(
+        task_id = "zsgd_acct_loan", 
+        component = "Dataset", 
+        writeOptions = {"writeMode" : "overwrite"}, 
+        table = {"name" : "zsgd_acct_loan", "sourceName" : "westpac_edw_views", "sourceType" : "Table"}
     )
