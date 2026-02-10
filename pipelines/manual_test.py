1f0d09157bc24d6474accd02a7805f5b0804ae38
cfinlayson@paxata.com <cfinlayson@paxata.com>
2026-02-10 09:46:11 +0000

    Add `refs_covid_repay_type` and `refs_repay_type_map` datasets from `westpac_efs` source to `manual_test` pipeline

--- pipelines/manual_test.py
+++ pipelines/manual_test.py
@@ -1,4 +1,16 @@
 with DAG():
+    refs_covid_repay_type = Task(
+        task_id = "refs_covid_repay_type", 
+        component = "Dataset", 
+        writeOptions = {"writeMode" : "overwrite"}, 
+        table = {"name" : "refs_covid_repay_type", "sourceName" : "westpac_efs", "sourceType" : "Table"}
+    )
+    refs_repay_type_map = Task(
+        task_id = "refs_repay_type_map", 
+        component = "Dataset", 
+        writeOptions = {"writeMode" : "overwrite"}, 
+        table = {"name" : "refs_repay_type_map", "sourceName" : "westpac_efs", "sourceType" : "Table"}
+    )
     manual_test__Union_1 = Task(
         task_id = "manual_test__Union_1", 
         component = "Model", 
